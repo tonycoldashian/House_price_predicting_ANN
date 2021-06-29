2a16f7a4144a0a10635cd41bdab7e50bbfa7b9e2
Aswin Antony <61707579+tonycoldashian@users.noreply.github.com>
2021-06-29 15:49:01 +0530

    Loss and Accuracy works

--- house_price_predictions.py
+++ house_price_predictions.py
@@ -1,3 +1,5 @@
+# importing CSV file dataset
+
 import pandas as pd
 df = pd.read_csv('housepricedata.csv')
 
@@ -6,17 +8,23 @@ dataset = df.values
 X = dataset[:,0:10]
 Y = dataset[:,10]
 
+# sklearn for preprocessing steps
+
+
 from sklearn import preprocessing
 
 min_max_scaler = preprocessing.MinMaxScaler()
 X_scale = min_max_scaler.fit_transform(X)
+
+# Splitting the data set into training dataset and testing dataset
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, test_size=0.3)
 X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
 
-#print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
+#Using Keras Library to implement the ANN
+# Activation functions are - RelU and Sigmoid
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -31,13 +39,18 @@ model.compile(optimizer='sgd',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+#training the model with training dataset
+
 hist = model.fit(X_train, Y_train,
           batch_size=32, epochs=100,
           validation_data=(X_val, Y_val))
 
+# Testing with Test dataset
 model.evaluate(X_test, Y_test)[1]
 
 import matplotlib.pyplot as plt
+
+#Loss in Predicted to Actual value is Plotted
 
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['val_loss'])
@@ -47,12 +60,12 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Val'], loc='upper right')
 plt.show()
 
-""" 
-plt.plot(hist.history['acc'])
-plt.plot(hist.history['val_acc'])
+# Accuracy is plotted 
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Val'], loc='lower right')
-plt.show() """
+plt.show()
 
